09df7de4d763694b9b8d1fddab6b52ad6b203dd0
Lingga Buwono <lingga.buwono.ft18@mail.umy.ac.id>
2020-12-21 18:44:26 +0700

    Add project files.

---
+++ ClientRest_042_Lingga/ClassData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Runtime.Serialization;
+using System.Net;
+using static ClientRest_042_Lingga.Program;
+
+namespace ClientRest_042_Lingga
+{
+    class ClassData
+    {
+        public void getData()
+        {
+            var json = new WebClient().DownloadString("http://localhost:1907/Mahasiswa");
+            var data = JsonConvert.DeserializeObject<List<Mahasiswa>>(json);
+
+            foreach(var mhs in data)
+            {
+                Console.WriteLine("NIM: " + mhs.nama);
+                Console.WriteLine("Nama: " + mhs.nim);
+                Console.WriteLine("Prodi: " + mhs.prodi);
+                Console.WriteLine("Angkatan: " + mhs.angkatan);
+            }
+            Console.ReadLine();
+        }
+    }
+}
+ 
